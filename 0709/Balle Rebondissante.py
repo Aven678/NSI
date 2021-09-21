68289d8cfec66dc36deecaaccaad5c7f3e224b4a
eleve <matthieu@justaven.xyz>
2021-09-21 13:22:23 +0200

    Ajout kill balles
    Signed-off-by: eleve <matthieu@justaven.xyz>

--- 0709/Balle Rebondissante.py
+++ 0709/Balle Rebondissante.py
@@ -19,11 +19,12 @@ class Balle:
         self.dx = randint(-10,10)
         self.dy = randint(-10,10)
         self.couleur = np.random.choice(range(256), size=3)
+        self.alive = True
     
     def draw(self):
         pygame.draw.circle(fenetre,self.couleur,(self.x,self.y),taille)
         
-    def avance(self, balles):
+    def avance(self, balles):        
         self.draw()
         
         if self.x < taille or self.x > longueur - taille :
@@ -41,6 +42,9 @@ class Balle:
         for balle in balles:
             if balle.id == self.id:
                 continue
+            
+            if balle.alive == False:
+                continue
             if self.distanceAB(balle.x,balle.y) < 2*taille:
                 self.dx,balle.dx = balle.dx,self.dx
                 self.dy,balle.dy = balle.dy,self.dy
@@ -52,7 +56,7 @@ class Balle:
 
 
 balles = [Balle(x) for x in range(int(input("Tu veux combien de balles ? ")))]
-print("Il y a",len(balles)," balles. ALLEZ ZÉ PARTI")
+print("Il y a",len(balles)," balles. ALLEZ ZÉ PARTI !")
 
 pygame.display.init()
 fenetre = pygame.display.set_mode((longueur, largeur))
@@ -61,15 +65,33 @@ fenetre.fill([0,0,0])
 while True:
     fenetre.fill([0,0,0])
     
+    balle_dead = 0
+    
     for balle in balles:
+        if balle.alive == False:
+            balle_dead += 1
+            continue
         balle.avance(balles)
     
+    if balle_dead == len(balles):
+        print("Et c'est gagné ! Bravo !")
+        pygame.display.quit()
+        sys.exit()
+        
     pygame.display.update()
     
     for event in pygame.event.get():
+        if event.type == MOUSEBUTTONDOWN and event.button == 1:
+            mouseX, mouseY = pygame.mouse.get_pos()
+            for b in balles:
+                if b.alive == False:
+                    continue
+                if (mouseX >= b.x-taille and mouseX <= b.x+taille) and (mouseY <= b.y+taille and mouseY >= b.y-taille):
+                    b.alive = False
+        
         if event.type == pygame.QUIT:
             print("Ah c'est la fin... Adieu.")
             pygame.display.quit()
             sys.exit()
             
-    time.sleep(0.01)
+    time.sleep(0.05)
