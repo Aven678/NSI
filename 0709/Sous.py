f66ff081d129b4b9ec64108a20a063f405ac95db
Aven <32174784+Aven678@users.noreply.github.com>
2021-09-21 09:00:09 +0200

    Delete Sous.py

--- 0709/Sous.py
+++
@@ -1,9 +0,0 @@
-class Fraction:
-    def __init__(self,den,num):
-        self.denominateur = den
-        self.numerateur = num
-        
-    def __str__(self):
-        if self.numerateur == 1:
-            return str(self.denominateur)
-        return str(self.denominateur)+"/"+str(self.numerateur)
