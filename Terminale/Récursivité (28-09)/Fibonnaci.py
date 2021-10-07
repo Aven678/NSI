f2b07a8de11fbb4ae585237d413e37d7fabf120c
eleve <matthieu@justaven.xyz>
2021-10-07 08:04:40 +0200

    Récursivité
    Signed-off-by: eleve <matthieu@justaven.xyz>

--- Terminale/Récursivité (28-09)/Fibonnaci.py
+++ Terminale/Récursivité (28-09)/Fibonnaci.py
@@ -7,4 +7,15 @@ def F(n):
     else:
         return F(n-1) + F(n-2)
 
-print(F(8))
+print(F(2))
+
+def F_imperatif(n, dict={0:0, 1:1}):
+    if n in dict:
+        return dict[n]
+
+    else:
+        dict[n] = F_imperatif(n-1, dict) + F_imperatif(n-2, dict)
+    
+    return F_imperatif(n-1, dict) + F_imperatif(n-2, dict)
+
+print(F_imperatif(10))
---
+++ Terminale/Récursivité (28-09)/test-fibo.py
@@ -0,0 +1,14 @@
+from rcviz import CallGraph,viz
+
+cg = CallGraph()
+
+@viz(cg)
+def fibo(n):
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    else:
+        return fibo(n-1)+fibo(n-2)
+
+cg.render()
