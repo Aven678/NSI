efa34d8409ab2f7ed18da960b276436c33d1dbbc
Matthieu <matthieu@justaven.xyz>
2021-11-09 08:03:07 +0100

    V4 Projet

--- Terminale/POO (07-09)/BalleV3.py
+++ Terminale/POO (07-09)/BalleV3.py
@@ -1,3 +1,7 @@
+#V4 Boules noires qui bougent, changement dans la règle du jeu : 
+# si on touche un boule noire plus grosse, on meurt et le jeu se termine.
+# Le but est de manger toutes les boules noires pour gagner
+
 import pygame, sys
 import time
 from pygame.locals import *
@@ -9,19 +13,23 @@ longueur,largeur = 1040,980
 
 taille = 10
 
+print("\nBonjour et bienvenue dans ce petit agar.io.")
+print("Le principe est simple: mangez un maximum de balles pour grossir et manger les boules noires.")
+print("Si vous êtes touché par une boule noire plus grosse que votre boule, le jeu s'arrête et vous avez perdu. \n")
+
 class Balle:
-    def __init__(self, taille = None, couleur = None, x=None, y = None, dx = None, dy = None):
-        self.x = randint(5, longueur) if x == None else x
-        self.y = randint(5, longueur) if y == None else y
+    def __init__(self, taille = None, couleur = None, dx = None, dy = None, x=None, y = None):
+        self.taille = 10 if taille == None else taille
+        self.x = randint(5, longueur-(10 if taille == None else taille)) if x == None else x
+        self.y = randint(5, longueur-(10 if taille == None else taille)) if y == None else y
         self.dx = dx if dx == None else dx
         self.dy = dy if dy == None else dy
         self.couleur = np.random.choice(range(230), size=3) if couleur == None else couleur
-        self.taille = 10 if taille == None else taille
     
     def draw(self):
         pygame.draw.circle(fenetre,self.couleur,(self.x,self.y),self.taille)
         
-    def avance(self):     #Uniquement utilisé par la balle contrôlée par l'utilisateur   
+    def avance(self, player_ball):     #Uniquement utilisé par la balle contrôlée par l'utilisateur   
         self.draw()
         
         if self.x < self.taille/2 or self.x > longueur - self.taille/2 :
@@ -33,21 +41,35 @@ class Balle:
         self.x += self.dx
         self.y += self.dy
         
-        self.collision()
+        self.collision(player_ball)
         
-    def collision(self):
+    def collision(self, player_ball): #Player_ball = False si il s'agit d'une boule noire
+        if player_ball == False:
+            if self.distanceAB(balle_joueur.x, balle_joueur.y) < self.taille:
+                if self.taille > balle_joueur.taille:
+                    print("Vous avez été éliminé par une boule noire!")
+                    arret()
+
+                else:
+                    boules_noires.remove(self)
+                    balle_joueur.taille += 0.2
+
+                    if len(boules_noires) == 0:
+                        print("Vous avez mangé toutes les boules noires ! \nVous avez gagné!")
+                        arret()
+
+            for boule in boules_noires: #Collision entre boules noires
+                if self.distanceAB(boule.x,boule.y) < 2*self.taille:
+                    self.dx,boule.dx = boule.dx,self.dx
+                    self.dy,boule.dy = boule.dy,self.dy
+
         for balle in balles:
             if balle == self:
                 continue
-        
+            
             if self.distanceAB(balle.x,balle.y) < self.taille:
                 self.taille += 0.2
                 balles.remove(balle)
-
-        for boule in boules_noires:
-            if self.distanceAB(boule.x,boule.y) < self.taille:
-                self.taille = self.taille//2
-                boules_noires.remove(boule)
         
     
     def distanceAB(self, xB, yB):
@@ -55,12 +77,12 @@ class Balle:
 
 
 balles = [Balle() for x in range(500)]
-boules_noires = [Balle(15, (0,0,0)) for x in range(10)]
+boules_noires = [Balle(randint(5, 30), (0,0,0), randint(-2,2), randint(-2,2)) for x in range(10)]
 
-balle_joueur = Balle(15,(255,0,0), longueur/2, largeur/2, 0, 0)
+balle_joueur = Balle(15,(255,0,0), 0, 0, longueur/2, largeur/2)
 
-print("Il y a",len(balles)," balles à manger. ALLEZ ZÉ PARTI !")
-print("Attention aux boules noires !")
+print("Il y a",len(balles)," balles dans la partie. ALLEZ ZÉ PARTI !")
+print("Attention aux", len(boules_noires),"boules noires!")
 
 pygame.display.init()
 fenetre = pygame.display.set_mode((longueur, largeur))
@@ -68,7 +90,7 @@ fenetre.fill([0,0,0])
 
 
 def arret():
-    print("Fin du jeu, merci d'avoir joué")
+    print("Fin du jeu, merci d'avoir joué.")
     pygame.display.quit()
     sys.exit()
 
@@ -82,7 +104,8 @@ while True:
         balle.draw()
 
     for boule in boules_noires:
-        boule.draw()
+        boule.avance(False)
+
 
     balle_joueur.draw()
         
@@ -92,7 +115,7 @@ while True:
         if event.type == KEYDOWN:
             if event.key == K_RIGHT or event.key == K_d:
                 pygame.key.set_repeat(40)
-                if balle_joueur.x < longueur + balle_joueur.taille:
+                if balle_joueur.x < longueur - balle_joueur.taille:
                     balle_joueur.x += 5
 
             if event.key == K_UP or event.key == K_z:
@@ -107,7 +130,7 @@ while True:
 
             if event.key == K_DOWN or event.key == K_s:
                 pygame.key.set_repeat(40)
-                if balle_joueur.y < largeur + balle_joueur.taille:
+                if balle_joueur.y < largeur - balle_joueur.taille:
                     balle_joueur.y += 5
 
             if event.key == K_ESCAPE:
@@ -116,7 +139,7 @@ while True:
         if event.type == pygame.QUIT:
             arret()
             
-    balle_joueur.avance()
+    balle_joueur.avance(True)
 
     if len(balles) < 500:
         for i in range(500-len(balles)):
