487dea961b18ad38b04b6dc55ff061721e5ccdf4
Matthieu <matthieu@justaven.xyz>
2021-12-09 08:14:35 +0100

    dictionnaires

--- Terminale/Exercices Pratiques/ex7.py
+++ Terminale/Exercices Pratiques/ex7.py
@@ -2,7 +2,11 @@
 
 def fibonnaci(n):
     resultats = {1:1, 2:1}
+
+    if n in resultats:
+        return resultats[n]
+
     for i in range(3,n+1):
         resultats[i] = resultats[i-1] + resultats[i-2]
         
-    print(resultats[n])
+    return resultats[n]
